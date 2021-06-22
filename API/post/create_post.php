847f8180e36ec0d1d3deb51e4dc1d8f78a0b3a4f
RusiraPathum <48381430+RusiraPathum@users.noreply.github.com>
2021-06-22 23:02:50 +0530

    Create Post API Part

--- API/post/create_post.php
+++ API/post/create_post.php
@@ -1,1 +1,22 @@
 <?php
+
+header('Access-Control-Allow_Origin: *');
+header('Content-Type: application/json');
+
+include_once "../../Config/db.php";
+include_once "../../Model/Post.php";
+
+$data = json_decode(file_get_contents('php://input'));
+
+$database = new db();
+$connection = $database->getConnection();
+$post = new Post($connection);
+
+$post->postTitle = $data->title;
+$post->postBody = $data->body;
+
+if ($post->createPost()){
+    echo json_encode(['msg' => '1']);
+}else{
+    echo json_encode(['msg' => '0']);
+}
